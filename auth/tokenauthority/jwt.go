ff0e1ac2ec06e60bf3a1c2e615d35de4fc0f21fc
Demetrius <25155822+Laelapa@users.noreply.github.com>
2025-05-13 19:55:04 +0300

    Add JWT issuance and validation methods to TokenAuthority

---
+++ auth/tokenauthority/jwt.go
@@ -0,0 +1,49 @@
+package tokenauthority
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func (t *TokenAuthority) IssueJWT(userID string) (signedToken string, err error) {
+
+	claims := jwt.RegisteredClaims{
+		Issuer:    t.jwtIssuer,
+		Subject:   userID,
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(t.jwtLifetime)),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err = token.SignedString([]byte(t.jwtSecret))
+	if err != nil {
+		return "", errors.New("failed to sign JWT" + err.Error())
+	}
+
+	return signedToken, nil
+}
+
+func (t *TokenAuthority) ValidateJWT(tokenString string) (subject string, err error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+
+		if token.Method != jwt.SigningMethodHS256 {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(t.jwtSecret), nil
+	})
+	if err != nil {
+		return "", errors.New("failed to parse JWT" + err.Error())
+	}
+
+	subject, err = token.Claims.GetSubject()
+	if err != nil {
+		return "", errors.New("failed to extract the subject from JWT" + err.Error())
+	}
+
+	return subject, nil
+}
