831a36be82b738527a1ac3ca54cd3956300c3c0b
Demetrius <25155822+Laelapa@users.noreply.github.com>
2025-05-24 17:05:50 +0300

    Create middleware for authenticating via JWT. Create helper function for stripping the "Bearer" prefix from the auth heder. Create helper function for converting string to pgtype.UUID
    WIP: Create HandlePostFood() handler method

--- internal/routes/handlers/handle_food_registry.go
+++ internal/routes/handlers/handle_food_registry.go
@@ -8,10 +8,39 @@ import (
 	"go.uber.org/zap"
 )
 
+type createFoodRequest struct {
+	Name                   string  `json:"name"`
+	Gtin                   string  `json:"gtin,omitempty"`
+	Category               string  `json:"category,omitempty"`
+	Description            string  `json:"description,omitempty"`
+	UnitType               string  `json:"unit_type"`	// Type of unit, e.g., "grams", "liters", "items", "portions" etc.
+	Quantity               int     `json:"quantity"`
+	PortionCount           int     `json:"portion_count"`	// Number of portions in the food item
+	ExpirationAfterOpening int     `json:"expiration_after_opening,omitempty"` // in days
+	NutrientsPerItem       bool    `json:"nutrients_per_item,omitempty"`	// true: per item/portion, false: per 100g
+	Calories               float64 `json:"calories,omitempty"`
+	Fats                   float64 `json:"fats,omitempty"`	
+	Saturated              float64 `json:"saturated,omitempty"` 
+	Carbs                  float64 `json:"carbs,omitempty"`
+	Sugars                 float64 `json:"sugars,omitempty"`
+	Protein                float64 `json:"protein,omitempty"`
+	Fiber                  float64 `json:"fiber,omitempty"`
+	Sodium                 float64 `json:"sodium,omitempty"`
+}
+
+type createFoodResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message,omitempty"`
+	ProductID string `json:"product_id,omitempty"`
+
 func (h *Handler) HandlePostFood(w http.ResponseWriter, r *http.Request) {
 
-	var rBody repository.CreateFoodEntryParams
+	var rBody createFoodRequest
 	ctx := r.Context()
+
+	// Extract user ID from context - this should be set by the authentication middleware
+	userID, ok := r.Context().Value("userID").(uuid.UUID)
+	if !ok ||  {
 
 	h.logger.LogRequestInfo("Inserting new food to the registry", r)
 
--- internal/routes/router.go
+++ internal/routes/router.go
@@ -35,16 +35,16 @@ func Setup(staticDir string, logger *logging.Logger, queries *repository.Queries
 	// -- mux.HandleFunc("GET /reset-password", h.HandleGetResetPassword)
 	// mux.HandleFunc("POST /reset-password", h.HandlePostResetPassword)
 
-	mux.HandleFunc("GET /food/id/{id}", h.HandleGetFoodById)
-	mux.HandleFunc("GET /food/gtin/{gtin}", h.HandleGetFoodByGtin)
+	// mux.HandleFunc("GET /food/id/{id}", h.HandleGetFoodById)
+	// mux.HandleFunc("GET /food/gtin/{gtin}", h.HandleGetFoodByGtin)
 
-	mux.HandleFunc("POST /food", h.HandlePostFood)
-	mux.HandleFunc("PUT /food/id/{id}", h.HandlePutFood)
-	mux.HandleFunc("DELETE /food/id/{id}", h.HandleDeleteFood)	
+	mux.Handle("POST /food", middleware.AuthenticateWithJWT(tokenAuthority, logge, h.HandlePostFood))
+	// mux.HandleFunc("PUT /food/id/{id}", h.HandlePutFood)
+	// mux.HandleFunc("DELETE /food/id/{id}", h.HandleDeleteFood)	
 
-	mux.HandleFunc("GET /foods", h.HandleGetFoods)
-	mux.HandleFunc("GET /foods/category/{category}", h.HandleGetFoodsByCategory)
-	mux.HandleFunc("GET /foods/name/{name}", h.HandleGetFoodsByName)
+	// mux.HandleFunc("GET /foods", h.HandleGetFoods)
+	// mux.HandleFunc("GET /foods/category/{category}", h.HandleGetFoodsByCategory)
+	// mux.HandleFunc("GET /foods/name/{name}", h.HandleGetFoodsByName)
 
 
 	return mux
