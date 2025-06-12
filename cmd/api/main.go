26d21985260d2a5160ae3039800a5697cae75227
Demetrius <25155822+Laelapa@users.noreply.github.com>
2025-06-12 03:22:09 +0300

    Implement Dockerfile and set up CD pipeline for fly.io

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,7 +18,6 @@ import (
 
 	"github.com/Laelapa/GoHome/logging"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 	"github.com/twmb/franz-go/pkg/kgo"
 	"go.uber.org/zap"
 
@@ -62,11 +61,11 @@ func run() error {
 	// Disabled for production, use your service's secrets instead
 	// Refer to the dotenv example file for the required environment variables
 	// Uncomment the following lines to load environment variables from a .env file in a local development environment
-
-	err := godotenv.Load()
-	if err != nil {
-		return fmt.Errorf("error loading .env file: %w", err)
-	}
+	//
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	return fmt.Errorf("error loading .env file: %w", err)
+	// }
 
 	// TODO: More configurable init based on .env options / Integrate viper
 
